f15487fbfe2e410a9080d7f1bbe13113304cc20a
Edgar <edgarkhurshudyan17@gmail.com>
2025-10-03 20:41:27 +0400

    Install and configure eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// @ts-check
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.js'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      'no-process-env': 'error',
+      '@typescript-eslint/no-unused-wars': 'error',
+      '@typescript-eslint/return-await': 'error',
+      '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/no-empty-function': 'error',
+      '@typescript-eslint/no-duplicate-type-constituents': 'error',
+    },
+  },
+);
