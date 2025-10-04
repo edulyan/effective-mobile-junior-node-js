0f1e1860386bdf69b7614cbdf196224342c2ad4e
Edgar <edgarkhurshudyan17@gmail.com>
2025-10-04 22:51:35 +0400

    Change error message for invalid mongoId

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,6 +3,6 @@ import { z } from 'zod';
 
 export const mongoIdSchema = z.object({
   id: z.string().refine((id) => mongoose.Types.ObjectId.isValid(id), {
-    message: 'Invalid MongoDB ObjectId',
+    message: 'Invalid identifier',
   }),
 });
